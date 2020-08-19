be8bedf4e51c7f1f00fa6fd0cde226f09d75e72d
Ant-sss <mimo.antownette.0429@gmail.com>
2020-08-19 12:42:47 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  has_mant :room_users
-  has_many :rooms, throught: :room_users
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
